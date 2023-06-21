fb3f588e6401d0b26d3c8ac725fb77a2bc0def7e
Brett Shirley <82629580+Brshirl@users.noreply.github.com>
2023-06-21 10:33:22 -0500

    Initial Commit

---
+++ Inventory/InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryApp.swift
+//  Inventory
+//
+//  Created by Brett Shirley on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
